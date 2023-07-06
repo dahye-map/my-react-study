e62c49befb693a87321afc441d75e1b41bb828cf
dahye <dahae5919@gmail.com>
2023-07-06 15:37:03 +0900

    update : counter function exam

--- react-exam1/src/App.js
+++ react-exam1/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
-import MyHeader from './MyHeader'
-import MyFooter from './MyFooter'
+import MyHeader from './MyHeader';
+import Counter from './Counter';
+import Container from './Container';
 
 function App() {
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+  }
   return (
-    <div className='App'>
-      <MyHeader />
-      <h1>안녕 리액트</h1>
-      <MyFooter />
-    </div>
+    <Container>
+      <div>
+        <MyHeader />
+        <Counter {...counterProps} />
+      </div>
+    </Container>
   );
 }
 
