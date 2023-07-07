913c60390bf97d575d4fcd2588f720a568adbf3c
dahye <dahae5919@gmail.com>
2023-07-07 16:30:47 +0900

    update: delete function update

--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -1,14 +1,26 @@
-const DiaryItem = ({ author, content, emotion, created_date }) => {
+const DiaryItem = ({ onDelete, ...it }) => {
   return (
     <div className="DiaryItem">
       <div className="info">
         <span>
-          작성자 : {author} | 감정점수 : {emotion}
+          작성자 : {it.author} | 감정점수 : {it.emotion}
         </span>
         <br />
-        <span className="date">{new Date(created_date).toLocaleString()}</span>
+        <span className="date">
+          {new Date(it.created_date).toLocaleString()}
+        </span>
       </div>
-      <div className="content">{content}</div>
+      <div className="content">{it.content}</div>
+      <button
+        onClick={() => {
+          console.log(it.id);
+          if (window.confirm(`${it.id}번째 일기를 정말 삭제하시겠습니까?`)) {
+            onDelete(it.id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
