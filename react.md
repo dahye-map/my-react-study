e2c0fef1364dd3a1e391768fa492f60cadb2a565
dahye <dahae5919@gmail.com>
2023-07-11 13:45:07 +0900

    update: edit function

--- react.md
+++ react.md
@@ -1,9 +1,15 @@
 # React App을 만드는 방법
+
 1. React.js
-node 기반의 javascript ui 라이브러리
+   node 기반의 javascript ui 라이브러리
 2. webpack
-다수의 자바스크립트 파일을 하나의 파일로 합쳐주는 모듈 번들 라이브러리
+   다수의 자바스크립트 파일을 하나의 파일로 합쳐주는 모듈 번들 라이브러리
 3. babel
-JSX등의 쉽고 직관적인 자바스크립트 문법을 사용할 수 있도록 해주는 라이브러리
+   JSX등의 쉽고 직관적인 자바스크립트 문법을 사용할 수 있도록 해주는 라이브러리
 
 Boiler Plate 이미 세팅 완료된 패키지
+
+### Lifecycle
+
+Mount -> Update -> UnMount
+ex) 초기화 작업 -> 예외 처리 작업 -> 메모리 정리 작업
--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -1,4 +1,34 @@
-const DiaryItem = ({ onDelete, ...it }) => {
+import { useRef, useState } from "react";
+
+const DiaryItem = ({ onEdit, onRemove, ...it }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const [localContent, setLocalContent] = useState(it.content);
+  const localContentInput = useRef();
+
+  const handleClickRemove = () => {
+    if (window.confirm(`${it.id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(it.id);
+    }
+  };
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(it.content);
+  };
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+    if (window.confirm(`${it.id}번 째 일기를 수정하시겠습니까?`)) {
+      onEdit(it.id, localContent);
+      toggleIsEdit();
+    }
+  };
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -10,17 +40,31 @@ const DiaryItem = ({ onDelete, ...it }) => {
           {new Date(it.created_date).toLocaleString()}
         </span>
       </div>
-      <div className="content">{it.content}</div>
-      <button
-        onClick={() => {
-          console.log(it.id);
-          if (window.confirm(`${it.id}번째 일기를 정말 삭제하시겠습니까?`)) {
-            onDelete(it.id);
-          }
-        }}
-      >
-        삭제하기
-      </button>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{it.content}</>
+        )}
+      </div>
+
+      {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>수정 취소</button>
+          <button onClick={handleEdit}>수정 완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleClickRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
     </div>
   );
 };
