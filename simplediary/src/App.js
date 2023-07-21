c9a3828507adb4d1d682938adc5e57608a7fc8c6
dahye <dahae5919@gmail.com>
2023-07-21 17:20:28 +0900

    update

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+import OptimizeTest from "./OptimizeTest";
 
 function App() {
   const [data, setData] = useState([]);
@@ -69,6 +70,7 @@ function App() {
 
   return (
     <div className="App">
+      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
       <div>전체 일기 : {data.length}</div>
       <div>기분 좋은 일기 갯수 : {goodCount}</div>
